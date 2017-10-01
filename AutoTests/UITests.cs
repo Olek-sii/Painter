3274a0a6cd1372c039a51d236c8e13c08ccfa441
Надежда Бердник <nadya.berdnik@mail.ru>
2017-10-02 01:30:03 +0300

    Небольшие изменения в тестах и добавление пакетов.

---
+++ AutoTests/UITests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestStack.White;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.Finders;
+using TestStack.White.UIItems.WindowItems;
+
+namespace AutoTests
+{
+    [TestClass]
+    public class UITests
+    {
+        public static Application application;
+
+        [TestCleanup]
+        public void TestTearDown()
+        {
+            application.Close();
+        }
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            application = Application.Launch(@"E:\CSharpDev\MyPreviousVersions\2017.09.22\Painter\...");
+            Assert.IsNotNull(application);
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
