5ddaf435db9b92d66f1beb62206d2e89ae8269ab
ITHope <nadya.berdnik@mail.ru>
2017-10-02 09:06:02 +0300

    Дополнено меню

--- Painter/Views/PMainMenu.cs
+++ Painter/Views/PMainMenu.cs
@@ -8,29 +8,68 @@ namespace Painter.Views
     {
 		private XCommand _xCommand = null;
 
-		private ToolStripButton _debugBtn;
-        private ToolStripButton _fileBtn;
-        private ToolStripButton _viewBtn;
-        private ToolStripButton _pluginsBtn;
-        private ToolStripButton _tabsBtn;
-        private ToolStripButton _propertiesBtn;
-        private ToolStripButton _preferencesBtn;
-        private ToolStripButton _helpBtn;
+        // MainMenu
+        private ToolStripMenuItem _debugBtn;
+        private ToolStripMenuItem _fileBtn;
+        private ToolStripMenuItem _viewBtn;
+        private ToolStripMenuItem _pluginsBtn;
+        private ToolStripMenuItem _tabsBtn;
+        private ToolStripMenuItem _propertiesBtn;
+        private ToolStripMenuItem _preferencesBtn;
+        private ToolStripMenuItem _helpBtn;
+
+        // MainMenu: File
+        private ToolStripMenuItem _newBtn;
+        private ToolStripMenuItem _openBtn;
+        private ToolStripMenuItem _saveBtn;
+        private ToolStripMenuItem _saveAsBtn;
+        private ToolStripMenuItem _closeTabBtn;
+        private ToolStripMenuItem _renameTabBtn;
+        private ToolStripMenuItem _openFromCloudBtn;
+        private ToolStripMenuItem _saveInCloudBtn;
+        private ToolStripMenuItem _exitBtn;
+
+        // MainMenu: View
+        private ToolStripMenuItem _elementsBtn;
+        private ToolStripMenuItem _propertiesFileBtn;
+
+        // MainMenu: Plug-ins
+        private ToolStripMenuItem _formatsBtn;
+        private ToolStripMenuItem _elementsPluginsBtn;
+
+        // MainMenu: Preferences
+        private ToolStripMenuItem _languageBtn;
+        private ToolStripMenuItem _skinBtn;
+
+        // MainMenu: Heop
+        private ToolStripMenuItem _aboutBtn;
 
         public PMainMenu(XCommand xCommand)
 		{
 			_xCommand = xCommand;
 
-			_debugBtn = new ToolStripButton(Localization.GetText("debug_text_id"), null, delegate { _xCommand.Debug(); });
-            _fileBtn = new ToolStripButton(Localization.GetText("file_text_id"), null);
-            _viewBtn = new ToolStripButton(Localization.GetText("view_text_id"), null);
-            _pluginsBtn = new ToolStripButton(Localization.GetText("plugins_text_id"), null);
-            _tabsBtn = new ToolStripButton(Localization.GetText("tabs_text_id"), null);
-            _propertiesBtn = new ToolStripButton(Localization.GetText("properties_text_id"), null);
-            _preferencesBtn = new ToolStripButton(Localization.GetText("preferences_text_id"), null);
-            _helpBtn = new ToolStripButton(Localization.GetText("help_text_id"), null);
+            // MainMenu
+            _debugBtn = new ToolStripMenuItem(Localization.GetText("debug_text_id"), null, delegate { _xCommand.Debug(); });
+            _fileBtn = new ToolStripMenuItem(Localization.GetText("file_text_id"));
+            _viewBtn = new ToolStripMenuItem(Localization.GetText("view_text_id"));
+            _pluginsBtn = new ToolStripMenuItem(Localization.GetText("plugins_text_id"));
+            _tabsBtn = new ToolStripMenuItem(Localization.GetText("tabs_text_id"));
+            _propertiesBtn = new ToolStripMenuItem(Localization.GetText("properties_text_id"));
+            _preferencesBtn = new ToolStripMenuItem(Localization.GetText("preferences_text_id"));
+            _helpBtn = new ToolStripMenuItem(Localization.GetText("help_text_id"));
 
+            // MainMenu: File
+            _newBtn = new ToolStripMenuItem(Localization.GetText("new_text_id"), null);
+            _openBtn = new ToolStripMenuItem(Localization.GetText("open_text_id"), null);
+            _saveBtn = new ToolStripMenuItem(Localization.GetText("save_text_id"), null);
+            _saveAsBtn = new ToolStripMenuItem(Localization.GetText("save_as_text_id"), null);
+            _closeTabBtn = new ToolStripMenuItem(Localization.GetText("close_tab_text_id"), null);
+            _renameTabBtn = new ToolStripMenuItem(Localization.GetText("rename_tab_text_id"), null);
+            _openFromCloudBtn = new ToolStripMenuItem(Localization.GetText("open_from_cloud_text_id"), null);
+            _saveInCloudBtn = new ToolStripMenuItem(Localization.GetText("save_in_text_id"), null);
+            _exitBtn = new ToolStripMenuItem(Localization.GetText("exit_text_id"), null);
 
+            // MainMenu
             Items.Add(_debugBtn);
             Items.Add(_fileBtn);
             Items.Add(_viewBtn);
@@ -39,6 +78,17 @@ namespace Painter.Views
             Items.Add(_propertiesBtn);
             Items.Add(_preferencesBtn);
             Items.Add(_helpBtn);
+
+            // MainMenu: FileItems
+            _fileBtn.DropDownItems.Add(_newBtn);
+            _fileBtn.DropDownItems.Add(_openBtn);
+            _fileBtn.DropDownItems.Add(_saveBtn);
+            _fileBtn.DropDownItems.Add(_saveAsBtn);
+            _fileBtn.DropDownItems.Add(_closeTabBtn);
+            _fileBtn.DropDownItems.Add(_renameTabBtn);
+            _fileBtn.DropDownItems.Add(_openFromCloudBtn);
+            _fileBtn.DropDownItems.Add(_saveInCloudBtn);
+            _fileBtn.DropDownItems.Add(_exitBtn);
 
             foreach (IPluginFigure item in PluginManager.figurePlugins)
 			{
@@ -50,7 +100,8 @@ namespace Painter.Views
 
 		private void Localization_OnLocalChange()
 		{
-			_debugBtn.Text = Localization.GetText("debug_text_id");
+            // MainMenu
+            _debugBtn.Text = Localization.GetText("debug_text_id");
             _fileBtn.Text = Localization.GetText("file_text_id");
             _viewBtn.Text = Localization.GetText("view_text_id");
             _pluginsBtn.Text = Localization.GetText("plugins_text_id");
@@ -58,6 +109,17 @@ namespace Painter.Views
             _propertiesBtn.Text = Localization.GetText("properties_text_id");
             _preferencesBtn.Text = Localization.GetText("preferences_text_id");
             _helpBtn.Text = Localization.GetText("help_text_id");
+
+            // MainMenu: FileItems
+            _newBtn.Text = Localization.GetText("new_text_id");
+            _openBtn.Text = Localization.GetText("open_text_id");
+            _saveBtn.Text = Localization.GetText("save_text_id");
+            _saveAsBtn.Text = Localization.GetText("save_as_text_id");
+            _closeTabBtn.Text = Localization.GetText("close_tab_text_id");
+            _renameTabBtn.Text = Localization.GetText("rename_tab_text_id");
+            _openFromCloudBtn.Text = Localization.GetText("open_from_cloud_text_id");
+            _saveInCloudBtn.Text = Localization.GetText("save_in_text_id");
+            _exitBtn.Text = Localization.GetText("exit_text_id");
         }
-	}
+    }
 }
