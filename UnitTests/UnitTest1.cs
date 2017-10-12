a01840a917139cdb5de3f669ad04821c592cc2d3
Olek-sii <oleksii.al@gmail.com>
2017-10-12 23:43:04 +0300

    delete unnesessary files

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -2,6 +2,8 @@
 using NUnit.Framework;
 using Painter.Controllers;
 using System.Drawing;
+using System.Windows.Forms;
+using Painter.Views;
 
 namespace UnitTests
 {
@@ -14,28 +16,30 @@ namespace UnitTests
 		public void SetUp()
 		{
 			xCommand = new XCommand();
-			xCommand.ActivePDraw = new Painter.Views.PDraw(xCommand);
+			xCommand.TabControl = new TabControl();
+			xCommand.TabControl.TabPages.Add(new PDraw(xCommand));
+			xCommand.TabControl.SelectedIndex = 0;
 		}
 
         [Test]
         public void TestSetLineWidth()
         {
 			xCommand.SetLineWidth(5);
-			Assert.AreEqual(5, xCommand.ActivePDraw._xData.lineWidth);
+			Assert.AreEqual(5, (xCommand.TabControl.TabPages[0] as PDraw)._xData.lineWidth);
         }
 
 		[Test]
 		public void TestSetColor()
 		{
 			xCommand.SetColor(Color.Red);
-			Assert.AreEqual(Color.Red, xCommand.ActivePDraw._xData.color);
+			Assert.AreEqual(Color.Red, (xCommand.TabControl.TabPages[0] as PDraw)._xData.color);
 		}
 
 		[Test]
 		public void TestSetType()
 		{
 			xCommand.SetType(Painter.Models.XData.FigureType.Ellipse);
-			Assert.AreEqual(Painter.Models.XData.FigureType.Ellipse, xCommand.ActivePDraw._xData.type);
+			Assert.AreEqual(Painter.Models.XData.FigureType.Ellipse, (xCommand.TabControl.TabPages[0] as PDraw)._xData.type);
 		}
 	}
 }
